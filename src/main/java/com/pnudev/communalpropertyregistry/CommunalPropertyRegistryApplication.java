c85044cff2a8a19e10f866d1a9d0f39833fc93db
Ihor Polataiko <igor.bogdanovich39@gmail.com>
2021-02-18 23:51:52 +0200

    Initialized project

---
+++ src/main/java/com/pnudev/communalpropertyregistry/CommunalPropertyRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.pnudev.communalpropertyregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunalPropertyRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunalPropertyRegistryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pnudev/communalpropertyregistry/CommunalPropertyRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnudev.communalpropertyregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunalPropertyRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
