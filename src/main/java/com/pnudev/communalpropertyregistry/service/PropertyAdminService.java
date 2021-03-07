0442f90754c6b56b4d6f6a45c76adb9b7daedfaa
vanya706 <vanya.mostoviy@gmail.com>
2021-03-07 10:25:18 +0200

    implements PropertyAdminDto and PropertyAdminFormDto

---
+++ src/main/java/com/pnudev/communalpropertyregistry/service/PropertyAdminService.java
@@ -0,0 +1,17 @@
+package com.pnudev.communalpropertyregistry.service;
+
+import com.pnudev.communalpropertyregistry.dto.PropertyAdminDto;
+import com.pnudev.communalpropertyregistry.dto.PropertyAdminFormDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.lang.Nullable;
+
+public interface PropertyAdminService {
+
+    Page<PropertyAdminDto> findAll(@Nullable String nameOrAddress, @Nullable Long categoryByPurposeId, @Nullable String propertyStatus, Pageable pageable);
+
+    PropertyAdminDto findById(Long id);
+
+    void save(PropertyAdminFormDto propertyAdminFormDto);
+
+}
--- src/main/java/com/pnudev/communalpropertyregistry/service/PropertyAdminServiceImpl.java
+++ src/main/java/com/pnudev/communalpropertyregistry/service/PropertyAdminServiceImpl.java
@@ -1,16 +1,22 @@
 package com.pnudev.communalpropertyregistry.service;
 
 import com.pnudev.communalpropertyregistry.domain.Property;
+import com.pnudev.communalpropertyregistry.dto.PropertyAdminDto;
+import com.pnudev.communalpropertyregistry.dto.PropertyAdminFormDto;
 import com.pnudev.communalpropertyregistry.exception.ServiceException;
 import com.pnudev.communalpropertyregistry.repository.PropertyDslRepository;
+import com.pnudev.communalpropertyregistry.repository.PropertyRepository;
+import com.pnudev.communalpropertyregistry.util.mapper.PropertyAdminMapper;
 import com.querydsl.core.types.Predicate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -20,7 +26,7 @@ import static com.pnudev.communalpropertyregistry.domain.QProperty.property;
 import static java.util.Objects.nonNull;
 
 @Service
-public class PropertyServiceImpl implements PropertyService {
+public class PropertyAdminServiceImpl implements PropertyAdminService {
 
     private static final Pattern pattern = Pattern.compile(".*position\":\\{\"lat\":(?<lat>[\\d\\.]+),\"lon\":(?<lon>[\\d\\.]+).*");
 
@@ -28,15 +34,24 @@ public class PropertyServiceImpl implements PropertyService {
 
     private final PropertyDslRepository propertyDslRepository;
 
+    private final PropertyRepository propertyRepository;
+
+    private final PropertyAdminMapper propertyAdminMapper;
+
     @Autowired
-    public PropertyServiceImpl(Environment environment, PropertyDslRepository propertyDslRepository) {
+    public PropertyAdminServiceImpl(Environment environment,
+                                    PropertyDslRepository propertyDslRepository,
+                                    PropertyRepository propertyRepository,
+                                    PropertyAdminMapper propertyAdminMapper) {
 
         this.environment = environment;
         this.propertyDslRepository = propertyDslRepository;
+        this.propertyRepository = propertyRepository;
+        this.propertyAdminMapper = propertyAdminMapper;
     }
 
     @Override
-    public Page<Property> findAll(String nameOrAddress, Long categoryByPurposeId, String propertyStatus, Pageable pageable) {
+    public Page<PropertyAdminDto> findAll(String nameOrAddress, Long categoryByPurposeId, String propertyStatus, Pageable pageable) {
 
         List<Predicate> predicates = new ArrayList<>();
 
@@ -50,10 +65,49 @@ public class PropertyServiceImpl implements PropertyService {
 
         if (nonNull(propertyStatus)) {
             predicates.add(property.propertyStatus
-                    .eq(String.valueOf(Property.PropertyStatus.valueOf(propertyStatus.toUpperCase()))));
+                    .eq(String.valueOf(Property.PropertyStatus.valueOf(propertyStatus))));
         }
 
-        return propertyDslRepository.findAll(pageable, predicates.toArray(Predicate[]::new));
+        Page<Property> propertiesPage = propertyDslRepository.findAll(pageable, predicates.toArray(Predicate[]::new));
+
+        return new PageImpl<>(propertyAdminMapper.mapToPropertyAdminDto(propertiesPage.getContent()),
+                pageable, propertiesPage.getTotalElements());
+    }
+
+    @Override
+    public PropertyAdminDto findById(Long id) {
+
+        Property property = propertyRepository.findById(id).orElseThrow(
+                () -> new ServiceException("Didn't find property by id"));
+
+        return propertyAdminMapper.mapToPropertyAdminDto(property);
+    }
+
+    @Override
+    public void save(PropertyAdminFormDto propertyAdminFormDto) {
+
+        Property property = Property.builder()
+                .id(propertyAdminFormDto.getId())
+                .imageUrl(propertyAdminFormDto.getImageUrl())
+                .address(propertyAdminFormDto.getAddress())
+                .propertyLocation(convertAddressToPropertyLocation(propertyAdminFormDto.getAddress()))
+                .name(propertyAdminFormDto.getName())
+                .categoryByPurposeId(propertyAdminFormDto.getCategoryByPurposeId())
+                .propertyStatus(propertyAdminFormDto.getPropertyStatus())
+                .area(propertyAdminFormDto.getArea())
+                .areaTransferred(propertyAdminFormDto.getAreaTransferred())
+                .balanceHolder(propertyAdminFormDto.getBalanceHolder())
+                .owner(propertyAdminFormDto.getOwner())
+                .leaseAgreementEndDate(propertyAdminFormDto.getLeaseAgreementEndDate())
+                .amountOfRent(propertyAdminFormDto.getAmountOfRent())
+                .isAreaTransferredPubliclyViewable(propertyAdminFormDto.isAreaTransferredPubliclyViewable())
+                .isBalanceHolderPubliclyViewable(propertyAdminFormDto.isBalanceHolderPubliclyViewable())
+                .isOwnerPubliclyViewable(propertyAdminFormDto.isOwnerPubliclyViewable())
+                .isLeaseAgreementEndDatePubliclyViewable(propertyAdminFormDto.isLeaseAgreementEndDatePubliclyViewable())
+                .isAmountOfRentPubliclyViewable(propertyAdminFormDto.isAmountOfRentPubliclyViewable())
+                .build();
+
+        propertyRepository.save(property);
     }
 
     private Property.PropertyLocation convertAddressToPropertyLocation(String address) {
