74e4e7ee6f78d4cbda5756be7588a716c988b3b9
Arthur Drabazha <jhorr2222@gmail.com>
2021-03-07 14:04:53 +0200

    Update pagination system

--- src/main/java/com/pnudev/communalpropertyregistry/dto/CategoryByPurposeDto.java
+++ src/main/java/com/pnudev/communalpropertyregistry/dto/CategoryByPurposeDto.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class CategoryByPurposeCreateDto {
+public class CategoryByPurposeDto {
 
     private String name;
 
