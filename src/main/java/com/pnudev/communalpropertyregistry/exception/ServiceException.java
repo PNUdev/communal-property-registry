3830d004d177ffda4baef5510f3b35ffd8e00e62
Arthur Drabazha <jhorr2222@gmail.com>
2021-03-11 12:06:17 +0200

    Fix comments

--- src/main/java/com/pnudev/communalpropertyregistry/exception/ServiceException.java
+++
@@ -1,9 +0,0 @@
-package com.pnudev.communalpropertyregistry.exception;
-
-public class ServiceException extends RuntimeException {
-
-    public ServiceException(String message) {
-        super(message);
-    }
-
-}
