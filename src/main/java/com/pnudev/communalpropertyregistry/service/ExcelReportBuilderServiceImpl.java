02626272ea657b5df671e51f2d5eeafc435a6205
SerhiiKlymchuk <klymchuk.buisness@gmail.com>
2021-03-25 18:28:28 +0200

    excel write rows impl

--- src/main/java/com/pnudev/communalpropertyregistry/service/ExcelReportBuilderServiceImpl.java
+++ src/main/java/com/pnudev/communalpropertyregistry/service/ExcelReportBuilderServiceImpl.java
@@ -1,25 +1,31 @@
 package com.pnudev.communalpropertyregistry.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pnudev.communalpropertyregistry.domain.Property;
+import com.pnudev.communalpropertyregistry.dto.response.AttachmentResponseDto;
+import com.pnudev.communalpropertyregistry.dto.response.PropertyResponseDto;
+import com.pnudev.communalpropertyregistry.exception.ServiceApiException;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.hssf.usermodel.HSSFFont;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Row;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
-import java.awt.print.Pageable;
+import java.io.OutputStream;
+import java.lang.reflect.Field;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Map;
+import java.util.List;
 import java.util.stream.IntStream;
 
+@Slf4j
 @Service
 public class ExcelReportBuilderServiceImpl implements ExcelReportBuilderService {
 
@@ -44,23 +50,28 @@ public class ExcelReportBuilderServiceImpl implements ExcelReportBuilderService 
         response.setHeader("Content-Disposition",
                 String.format("attachment; filename=%s.xls", getFileName()));
 
-        writeHeaderRow();
-        writeDataRows(searchQuery, propertyStatus, categoryByPurposeId);
+        try (OutputStream outputStream = response.getOutputStream()) {
+            Pageable wholePage = PageRequest.of(0, Integer.MAX_VALUE);
+            List<PropertyResponseDto> properties = propertyService.findPropertiesBySearchQuery(
+                    searchQuery, propertyStatus, categoryByPurposeId, wholePage).toList();
 
-        ServletOutputStream outputStream = response.getOutputStream();
+            writeHeaderRow();
+            writeDataRows(properties);
 
-        workbook.write(outputStream);
-        workbook.close();
-
-        outputStream.close();
+            workbook.write(outputStream);
+            workbook.close();
+        } catch (Exception e) {
+            log.error("Error while generating Excel report", e);
+            throw new ServiceApiException("Помилка при генерації Excel звіту");
+        }
     }
 
     // Write header rows with font-style: bold
     private void writeHeaderRow() {
 
-        String[] columnNames = {"ID", "Назва", "Посилання на зображення", "Адреса", "Довгота", "Широта",
-                "Назва категорії", "Площа", "Площа для продажу", "Балансоутримувач", "Власник",
-                "Дата завершення договору", "Сума(грн)", "Прикріплення"};
+        String[] columnNames = {"ID", "Посилання на зображення", "Адреса", "Довгота", "Широта", "Назва",
+                "Назва категорії", "Статус", "Площа", "Площа для продажу", "Балансоутримувач", "Власник",
+                "Дата завершення договору", "Сума(грн)", "Посилання на прикріплення"};
 
         Row row = sheet.createRow(0);
         CellStyle cellBoldStyle = getCellBoldStyle();
@@ -75,38 +86,68 @@ public class ExcelReportBuilderServiceImpl implements ExcelReportBuilderService 
 
     }
 
-    private void writeDataRows(String searchQuery, String propertyStatus, Long categoryByPurposeI) {
+    private void writeDataRows(List<PropertyResponseDto> properties) {
 
-//        List<PropertyResponseDto> properties = propertyService.findPropertiesBySearchQuery(
-//               searchQuery,  propertyStatus,  categoryByPurposeId, Pageable.unpaged());
-
-        ObjectMapper oMapper = new ObjectMapper();
-        int propertiesSize = 10;   // temporary
-        int columnsAmount = 14;    // temporary
-
-
-
-
-
-        IntStream.range(0, propertiesSize).forEach(
+        IntStream.range(0, properties.size()).forEach(
                 idx -> {
-                    Row row = sheet.createRow(idx+1);
-//                    Property property = properties.get(idx);
-//                    Map<String, Object> propertyMap = oMapper.convertValue(property, Map.class);
+                    Row row = sheet.createRow(idx + 1);
+                    PropertyResponseDto property = properties.get(idx);
 
+                    System.out.println(property.toString());
 
-                    for(int i =0; i < columnsAmount; i++){
-                        Cell cell = row.createCell(i);
+                    Field[] allFields = property.getClass().getDeclaredFields();
 
-                        cell.setCellValue("YES");
+                    //Write cells of property
+                    int cellCount = 0;
 
-                        sheet.autoSizeColumn(i);
+                    for (Field field : allFields) {
+                        field.setAccessible(true);
+                        Object value;
+
+                        try {
+                            value = field.get(property);
+                        } catch (Exception e) {
+                            log.error("Error while generating Excel report", e);
+                            throw new ServiceApiException("Помилка при генерації Excel звіту");
+                        }
+
+                        Cell cell = row.createCell(cellCount);
+
+                        if (value == null) {
+                            cell.setCellValue("-");
+                        } else if (value instanceof Property.PropertyLocation) {
+                            Property.PropertyLocation location = (Property.PropertyLocation) value;
+
+                            cell.setCellValue(location.getLon());
+
+                            row.createCell(++cellCount)
+                                    .setCellValue(location.getLat());
+                        } else if (value instanceof List<?>) {
+
+                            if (((List<?>) value).isEmpty()) {
+                                cell.setCellValue("-");
+                            } else {
+                                for (Object element : (List<?>) value) {
+                                    if (element instanceof AttachmentResponseDto) {
+                                        String link = ((AttachmentResponseDto) element).getLink();
+
+                                        row.createCell(cellCount++).setCellValue(link);
+                                    }
+                                }
+                            }
+
+                        } else {
+                            cell.setCellValue(value.toString());
+                        }
+
+                        cellCount++;
                     }
+
                 }
         );
     }
 
-    private String getFileName(){
+    private String getFileName() {
         DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
         String currentDateTime = dateFormatter.format(new Date());
 
